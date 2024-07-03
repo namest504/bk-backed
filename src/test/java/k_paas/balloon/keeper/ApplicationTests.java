9b6fcdf91473d81765beb96b0d4ffe475e1f23a7
Icecoff22 <icecoff2222@gmail.com>
2024-07-04 01:04:19 +0900

    [INIT]

---
+++ src/test/java/k_paas/balloon/keeper/ApplicationTests.java
@@ -0,0 +1,13 @@
+package k_paas.balloon.keeper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
