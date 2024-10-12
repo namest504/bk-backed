ce6369505b653bbf89939b6cea6a51fe782af6b8
lim1t <namest504@gmail.com>
2024-10-13 01:25:17 +0900

    test: 네임 스페이스 테스트

--- src/main/java/k_paas/balloon/keeper/api/domain/BalloonPositionController.java
+++ src/main/java/k_paas/balloon/keeper/api/domain/BalloonPositionController.java
@@ -2,17 +2,21 @@ package k_paas.balloon.keeper.api.domain;
 
 import static org.springframework.http.HttpStatus.OK;
 
+import java.net.URI;
 import java.util.List;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
 
 @RestController
 @RequestMapping("/api")
 public class BalloonPositionController {
 
     private final BalloonPositionService balloonPositionService;
+    private final RestTemplate restTemplate;
 
     public BalloonPositionController(BalloonPositionService balloonPositionService) {
         this.balloonPositionService = balloonPositionService;
@@ -24,5 +28,15 @@ public class BalloonPositionController {
 
         return ResponseEntity.status(OK)
                 .body(result);
+    }
+
+    @GetMapping("/ns")
+    public String test(){
+        URI uri = UriComponentsBuilder.fromHttpUrl("http://my-simulation-server-service:8001/climate-data?object_name=climate/climate_data_09:17:57.737912968.csv")
+                .build()
+                .toUri();
+        ResponseEntity<String> response = restTemplate.getForEntity(uri.toString(), String.class);
+
+        return response.getStatusCode() + " : " + response.getBody();
     }
 }
