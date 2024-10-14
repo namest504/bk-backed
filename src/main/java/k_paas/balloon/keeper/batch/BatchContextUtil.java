4cd02f16a4241bdf98373f7ae4d2fad138511581
lim1t <namest504@gmail.com>
2024-10-14 23:18:34 +0900

    refactor: BatchContext Util 리팩토링

--- src/main/java/k_paas/balloon/keeper/batch/BatchContextUtil.java
+++ src/main/java/k_paas/balloon/keeper/batch/BatchContextUtil.java
@@ -2,18 +2,27 @@ package k_paas.balloon.keeper.batch;
 
 import org.springframework.batch.core.StepExecution;
 import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.item.ExecutionContext;
 
 public class BatchContextUtil {
 
-    public static String getCurrentBatchContext(ChunkContext chunkContext, String value) {
-        return getCurrentBatchContext(chunkContext.getStepContext().getStepExecution(), value);
+    public static String getCurrentBatchContext(ChunkContext chunkContext, String key) {
+        return getCurrentBatchContext(getStepExecution(chunkContext), key);
     }
 
-    public static String getCurrentBatchContext(StepExecution stepExecution, String value) {
-        return (String) stepExecution.getJobExecution().getExecutionContext().get(value);
+    public static String getCurrentBatchContext(StepExecution stepExecution, String key) {
+        return (String) getExecutionContext(stepExecution).get(key);
     }
 
-    public static void addContextData(ChunkContext chunkContext, String timestamp, String timestamp1) {
-        chunkContext.getStepContext().getStepExecution().getJobExecution().getExecutionContext().put(timestamp, timestamp1);
+    public static void addContextData(ChunkContext chunkContext, String key, String value) {
+        getExecutionContext(getStepExecution(chunkContext)).put(key, value);
+    }
+
+    private static StepExecution getStepExecution(ChunkContext chunkContext) {
+        return chunkContext.getStepContext().getStepExecution();
+    }
+
+    private static ExecutionContext getExecutionContext(StepExecution stepExecution) {
+        return stepExecution.getJobExecution().getExecutionContext();
     }
 }
