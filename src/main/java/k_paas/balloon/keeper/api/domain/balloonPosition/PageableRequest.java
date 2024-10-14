b4609bc2ea887d5272b6bc4f6db759dc2788eda5
lim1t <namest504@gmail.com>
2024-10-15 01:23:22 +0900

    fix: Pageable 객체 정렬값 수정 가능하게 변경

--- src/main/java/k_paas/balloon/keeper/api/domain/balloonPosition/PageableRequest.java
+++ src/main/java/k_paas/balloon/keeper/api/domain/balloonPosition/PageableRequest.java
@@ -5,7 +5,6 @@ import org.springframework.boot.context.properties.bind.DefaultValue;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
-import org.springframework.data.domain.Sort.Direction;
 
 public record PageableRequest(
         @Min(1)
@@ -22,22 +21,13 @@ public record PageableRequest(
         @DefaultValue("desc")
         String sortDirection
 ) {
-    public Pageable toPageable(String sortBy) {
-        Sort sort = createSort(sortBy);
+    public Pageable toPageable() {
+        Sort sort = Sort.by(Sort.Direction.fromString(sortDirection), sortBy);
         return PageRequest.of(page - 1, size, sort);
     }
 
-    private Sort createSort(String properties) {
-        Sort initialSort = Sort.by(Sort.Direction.fromString(sortDirection), sortBy);
-        return initialSort.and(Sort.by(Sort.Direction.DESC, properties));
-    }
-
-    private Sort createSort(String properties, String dir) {
-        Sort initialSort = Sort.by(createSortDirection(dir), sortBy);
-        return initialSort.and(Sort.by(Sort.Direction.DESC, properties));
-    }
-
-    private Direction createSortDirection(String dir) {
-        return Direction.valueOf(dir);
+    public Pageable toPageable(String sortBy) {
+        Sort sort = Sort.by(Sort.Direction.fromString(sortDirection), sortBy);
+        return PageRequest.of(page - 1, size, sort);
     }
 }
