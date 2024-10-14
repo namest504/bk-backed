413b7cc8f5229ade8f87fc24b7f41a6c764a3c99
lim1t <namest504@gmail.com>
2024-10-14 17:12:07 +0900

    feat: api 요청 임시 로그 추가

--- src/main/java/k_paas/balloon/keeper/api/domain/BalloonPositionController.java
+++ src/main/java/k_paas/balloon/keeper/api/domain/BalloonPositionController.java
@@ -2,42 +2,30 @@ package k_paas.balloon.keeper.api.domain;
 
 import static org.springframework.http.HttpStatus.OK;
 
-import java.net.URI;
 import java.util.List;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
-import org.springframework.web.util.UriComponentsBuilder;
 
 @RestController
 @RequestMapping("/api")
+@Slf4j
 public class BalloonPositionController {
 
     private final BalloonPositionService balloonPositionService;
-    private final RestTemplate restTemplate;
 
-    public BalloonPositionController(BalloonPositionService balloonPositionService, RestTemplate restTemplate) {
+    public BalloonPositionController(BalloonPositionService balloonPositionService) {
         this.balloonPositionService = balloonPositionService;
-        this.restTemplate = restTemplate;
     }
 
     @GetMapping("/balloons")
     public ResponseEntity<List<BalloonPosition>> getAllConcurrentBalloonPosition() {
         final List<BalloonPosition> result = balloonPositionService.findAll();
 
+        log.info("/api/balloons excuted");
         return ResponseEntity.status(OK)
                 .body(result);
-    }
-
-    @GetMapping("/ns")
-    public String test(){
-        URI uri = UriComponentsBuilder.fromHttpUrl("http://my-simulation-server-service:8001/climate-data?object_name=climate/climate_data_09:17:57.737912968.csv")
-                .build()
-                .toUri();
-        ResponseEntity<String> response = restTemplate.getForEntity(uri.toString(), String.class);
-
-        return response.getStatusCode() + " : " + response.getBody();
     }
 }
