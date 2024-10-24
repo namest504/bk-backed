906fd9124b27a922bc9d927f54f361ed3436751a
lim1t <namest504@gmail.com>
2024-10-25 00:48:55 +0900

    fix: 생성자 추가

--- src/main/java/k_paas/balloon/keeper/api/domain/balloonPosition/BalloonReportWithCount.java
+++ src/main/java/k_paas/balloon/keeper/api/domain/balloonPosition/BalloonReportWithCount.java
@@ -6,4 +6,10 @@ public record BalloonReportWithCount(
         String streetAddress,
         Long reportCount
 ) {
+    public BalloonReportWithCount(Double centerLatitude, Double centerLongitude, String streetAddress, Long reportCount) {
+        this.centerLatitude = centerLatitude;
+        this.centerLongitude = centerLongitude;
+        this.streetAddress = streetAddress;
+        this.reportCount = reportCount;
+    }
 }
