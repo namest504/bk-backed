a9fb6004c593719ce845acd80f3e24d65da9ae7a
lim1t <namest504@gmail.com>
2024-10-31 19:13:30 +0900

    fix: swagger api key 전역 컴포넌트 설정 수정

--- src/main/java/k_paas/balloon/keeper/global/config/SwaggerConfig.java
+++ src/main/java/k_paas/balloon/keeper/global/config/SwaggerConfig.java
@@ -3,6 +3,8 @@ package k_paas.balloon.keeper.global.config;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.media.StringSchema;
+import io.swagger.v3.oas.models.parameters.Parameter;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.servers.Server;
@@ -16,20 +18,20 @@ public class SwaggerConfig {
     public OpenAPI openAPI() {
         final String securitySchemeName = "bearerAuth";
         return new OpenAPI()
-                .addSecurityItem(new SecurityRequirement().addList(securitySchemeName))
-                .components(
-                        new Components()
-                                .addSecuritySchemes(securitySchemeName,
-                                        new SecurityScheme()
-                                                .name("BK-API-KEY")
-                                                .type(SecurityScheme.Type.APIKEY)
-                                                .in(SecurityScheme.In.HEADER)
-                                )
-                )
+                .components(new Components()
+                        .addSecuritySchemes("api_key", new SecurityScheme()
+                                .type(SecurityScheme.Type.APIKEY)
+                                .in(SecurityScheme.In.HEADER)
+                                .name("BK-API-KEY"))
+                        .addParameters("BK-API-KEY", new Parameter()
+                                .in("header")
+                                .name("BK-API-KEY")
+                                .description("API Key")
+                                .required(true)
+                                .schema(new StringSchema())))
                 .info(apiInfo())
-                .addServersItem(
-                        new Server().url("/")
-                );
+                .addServersItem(new Server().url("/"))
+                .addSecurityItem(new SecurityRequirement().addList("api_key"));
     }
 
     private Info apiInfo() {
