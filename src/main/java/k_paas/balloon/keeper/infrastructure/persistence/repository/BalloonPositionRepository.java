ee5a55691267d3f9bc1052db48e66b938cd69674
icecoff22 <icecoff2222@gmail.com>
2024-10-31 11:22:38 +0900

    fix : 풍선 위치 쿼리문 중 주석 에러 픽스

--- src/main/java/k_paas/balloon/keeper/infrastructure/persistence/repository/BalloonPositionRepository.java
+++ src/main/java/k_paas/balloon/keeper/infrastructure/persistence/repository/BalloonPositionRepository.java
@@ -8,10 +8,13 @@ import java.util.List;
 
 public interface BalloonPositionRepository extends JpaRepository<BalloonPosition, Long> {
 
-    @Query("""
-            SELECT bp 
-            FROM BalloonPosition bp 
+//    @Query("""
+//            SELECT bp
+//            FROM BalloonPosition bp
 //            WHERE FUNCTION('TIMESTAMPDIFF', HOUR, bp.startPredictionTime, CURRENT_TIMESTAMP) <= 12 ORDER BY bp.startPredictionTime DESC
-            """)
+//            """)
+//    List<BalloonPosition> findPositionsWithinLast12Hours();
+
+    @Query("SELECT bp FROM BalloonPosition bp")
     List<BalloonPosition> findPositionsWithinLast12Hours();
 }
